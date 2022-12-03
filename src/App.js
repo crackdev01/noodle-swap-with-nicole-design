c27fd05047d01205a1cf18b3e7d7734fbda8296f
hiroshi <blackbox0823@gmail.com>
2022-12-03 11:14:14 +0900

    add landing page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Landing from './pages/Landing/index';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Landing />
+    </div>
+  );
+}
